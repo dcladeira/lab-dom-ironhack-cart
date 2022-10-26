316e50694bf3925e7a456567ba58eab45ed64efb
Daniel Carvalho Ladeira <daniel.ladeira@hotmail.com>
2022-10-26 17:03:40 -0300

    lab done til iteration 3, missing bonus

--- js/index.js
+++ js/index.js
@@ -1,23 +1,35 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
   //... your code goes here
+  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector(".price span");
+  const quantity = product.querySelector(".quantity input");
+  const subtotalPrice = (Number(price.textContent) * quantity.value).toFixed(2)
+  const subtotal = product.querySelector(".subtotal span");
+  subtotal.innerText = subtotalPrice;
+  return Number(subtotalPrice);
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const products = document.getElementsByClassName("product");
+  let totalPrice = 0;
+  for (let i = 0; i < products.length; i++) {
+    totalPrice += updateSubtotal(products.item(i));
+  }
 
   // ITERATION 3
   //... your code goes here
+  const total = document.getElementById("total-value");
+  total.innerText = `Total: \$${totalPrice.toFixed(2)}`;
 }
 
 // ITERATION 4
