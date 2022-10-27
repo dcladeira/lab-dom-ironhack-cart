85e88bfd25966add28712eaf5820b02c8c4a85b9
Daniel Carvalho Ladeira <daniel.ladeira@hotmail.com>
2022-10-27 17:39:55 -0300

    lab done, including bonus. however, if i remove all itens and try to add other, it doesn't work. I still have to fix this.

--- js/index.js
+++ js/index.js
@@ -38,17 +38,60 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  productTable = target.parentNode.parentNode.parentNode;
+  productTable.removeChild(target.parentNode.parentNode);
+  calculateAll()
 }
 
 // ITERATION 5
 
-function createProduct() {
+function createProduct(event) {
   //... your code goes here
+  const target = event.currentTarget;
+  lineNewProduct = target.parentNode.parentNode;
+  productName = lineNewProduct.querySelector("[type=text]").value;
+  productPrice = Number(lineNewProduct.querySelector("[type=number]").value).toFixed(2);
+  // productTable = document.getElementsByTagName("tbody");
+  // Não funcionaou porque pega um HTML Collection, ao invés do tbody
+  productTable = document.querySelector(".product").parentNode
+  const row = document.createElement("tr");
+  row.className = "product"
+  row.innerHTML = ` <td class="name">
+                      <span>${productName}</span>
+                    </td>
+                    <td class="price">$<span>${productPrice}</span></td>
+                    <td class="quantity">
+                      <input type="number" value="0" min="0" placeholder="Quantity" />
+                    </td>
+                    <td class="subtotal">$<span>0</span></td>
+                    <td class="action">
+                      <button class="btn btn-remove">Remove</button>
+                    </td>`
+  // Outra solução possível seria criar cada elemento, mas como as
+  // colunas são diferentes entre si, não seria usar um loop como esse,
+  // teria que criar coluna por coluna, adicionando cada atributo individualmente.
+  // for(let j = 0; j <= 4; j ++) {
+  //   const cell = document.createElement("td");
+  //   const cellText = document.createTextNode(`cell in column ${j}`);
+  //   cell.appendChild(cellText);
+  //   row.appendChild(cell);
+  // }
+  productTable.appendChild(row);
+  removeBtn = row.querySelector(".btn-remove");
+  removeBtn.addEventListener("click", removeProduct);
+  lineNewProduct.querySelector("[type=text]").value = "";
+  lineNewProduct.querySelector("[type=number]").value = 0;
+  calculateAll();
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
+  const removeBtn = document.getElementsByClassName('btn btn-remove');
+  for (i = 0; i < removeBtn.length; i++) {
+    removeBtn.item(i).addEventListener('click', removeProduct);
+  }
+  const createBtn = document.getElementById('create');
+  createBtn.addEventListener('click', createProduct)
   //... your code goes here
 });
